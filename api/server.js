0c6583a5aec61bd47d1a1f929de11c445271f59e
arturolei <artie.lei@gmail.com>
2019-09-14 20:36:23 -0700

    Ran npm install

--- api/server.js
+++ api/server.js
@@ -3,6 +3,14 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
+const server = express();
+
+const authRouter = require('../auth/auth-router.js')
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
 
 server.use('/api/login', authRouter);
 
