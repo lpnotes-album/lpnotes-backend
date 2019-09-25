a36cec0be2d0580c0d06488335f791c9ab17ea13
arturolei <artie.lei@gmail.com>
2019-09-24 22:05:34 -0700

    Starting postgres stuff

--- knexfile.js
+++ knexfile.js
@@ -21,5 +21,15 @@ module.exports = {
     seeds: {
       directory: './database/seeds',
     },
-  },
+  }, 
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'dbmigrations',
+    },
+    seeds: { directory: './database/seeds' },
+  }
 };
